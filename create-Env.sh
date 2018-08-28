9b20a9327ea2cc584fd6a35e89839a385c1aa52b
jai151990 <41561661+jai151990@users.noreply.github.com>
2018-08-28 10:46:29 -0700

    Add files via upload

---
+++ create-Env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cont_count=$1
+echo "creating $cont_count containers.."
+sleep 2;
+for i in `seq $cont_count`
+do
+	echo "================================="
+	echo "creating www.gamutkart$i container..."
+	sleep 1
+	docker run --name www.gamutkart$i -d -it --rm jaisriram111/shankar  /bin/bash
+	echo "www.gamutkart$i container has been created!"
+	echo "================================="
+done
+docker inspect -f '{{.NetworkSettings.Networks.bridge.IPAddress}}' `docker ps -q` > ip.txt
+
