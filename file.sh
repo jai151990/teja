820fdb0e890ac607ff089dd719de6bd102e990c6
jai151990 <41561661+jai151990@users.noreply.github.com>
2018-08-27 20:56:12 -0700

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cp target/student.war /home/siva/soft/apache-tomcat-8.5.32/webapps
 # /home/siva/soft/apache-tomcat-8.5.32/bin/startup.sh 
 export JAVA_HOME=/home/siva/soft/jdk1.8.0_171/bin ;/home/siva/soft/apache-tomcat-8.5.32/bin/startup.sh
